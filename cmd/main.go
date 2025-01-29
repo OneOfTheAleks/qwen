9eba50ca26d1f3e29c0315fe77993ff6094e7729
ritd <github.ritd@gmail.com>
2025-01-29 20:06:41 +0300

    first

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
+func main() {
+	// Загружаем переменные окружения из файла .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Ошибка при загрузке .env файла: %v", err)
+	}
+	apiKey := os.Getenv("API_KEY")
+	fmt.Printf("API Key: %s\n", apiKey)
 }
