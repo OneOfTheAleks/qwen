3c36e0e8d28839538a4b8b1eb4000c9a4d61ca08
ritd <github.ritd@gmail.com>
2025-01-29 20:33:55 +0300

    first

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/go-resty/resty/v2"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
 )
+
+type RequestBody struct {
+	Prompt string `json:"prompt"`
+}
+
+type ResponseBody struct {
+	Output string `json:"output"`
+}
 
 func main() {
 	// Загружаем переменные окружения из файла .env
@@ -15,4 +25,30 @@ func main() {
 	}
 	apiKey := os.Getenv("API_KEY")
 	fmt.Printf("API Key: %s\n", apiKey)
+
+	client := resty.New()
+
+	requestBody := RequestBody{
+		Prompt: "Привет, как дела?",
+	}
+
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Authorization", "Bearer "+apiKey).
+		SetBody(requestBody).
+		Post("https://dashscope-intl.aliyuncs.com/compatible-mode/v1")
+
+	if err != nil {
+		log.Fatalf("Error making request: %v", err)
+	}
+
+	var responseBody ResponseBody
+	body := resp.Body()
+	err = json.Unmarshal(body, &responseBody) //
+	if err != nil {
+		log.Fatalf("Error unmarshalling response: %v", err)
+	}
+
+	fmt.Println("Response:", responseBody.Output)
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module qwen
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-resty/resty/v2 v2.16.5
+	github.com/joho/godotenv v1.5.1
+)
+
+require golang.org/x/net v0.33.0 // indirect
